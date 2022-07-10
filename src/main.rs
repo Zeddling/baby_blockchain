3d08fdd6f6888a26e1de609ca2de5f26f29e1d27
zeddling <victorzeddys@gmail.com>
2022-07-10 21:15:02 +0300

    Added keypair and signature implementations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod keysig;
+
+use keysig::KeySig;
+
 fn main() {
-    println!("Hello, world!");
+    let keysig = KeySig::new();
+
+    println!("Keypair\n{}", keysig.to_string());
+
+    let data = b"Hello World";
+
+    let signature = keysig.sign(b"Hello World");
+
+    println!("Is it ours? {}", keysig.verify(data, &signature));
 }
